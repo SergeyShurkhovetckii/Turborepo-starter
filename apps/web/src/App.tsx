7195095602e4e0fc04df6c54192640d67e14224f
Sergey Shurkhovetskii <sshurkhovetskii@gmail.com>
2023-04-06 14:51:18 +0400

    Functional Monorepo

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "flowbite-react";
+
+function App() {
+  return (
+    <div className="flex justify-center mt-7">
+      <Button>Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
