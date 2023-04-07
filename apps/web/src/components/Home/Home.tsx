be3d7c73e042dcbcc41682d75cf01650e399ccec
Sergey Shurkhovetskii <sshurkhovetskii@gmail.com>
2023-04-07 17:56:36 +0400

    add Layout

--- apps/web/src/components/Home/Home.tsx
+++ apps/web/src/components/Home/Home.tsx
@@ -1,7 +1,9 @@
 export const Home = () => {
     return (
-        <div className="flex text-3xl w-justify-center bg-slate-800">
-            <button>Home</button>
+        <div className="px-5 lg:pt-[75px] lg:pb-12 ">
+            <h1 className="text-[40px] font-bold leading-[48px] lg:text-[64px] lg:leading-[72px]">
+                Home page
+            </h1>
         </div>
     );
 };
